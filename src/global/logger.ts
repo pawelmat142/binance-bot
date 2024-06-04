85890a9621775f47a668c0b7a8f9e5f4b1f079b9
pawelmat142 <pawelmat142@gmail.com>
2024-06-04 15:45:20 +0200

    set timezone in logger - there is 2h offset at the moment

--- src/global/logger.ts
+++ src/global/logger.ts
@@ -3,18 +3,26 @@ import * as DailyRotateFile from 'winston-daily-rotate-file';
 import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
 import { createLogger } from 'winston';
 
+import * as moment from 'moment-timezone';
+
+const timeZone = 'Europe/Warsaw';
+
+const customTimestamp = winston.format((info) => {
+  info.timestamp = moment().tz(timeZone).format('YYYY-MM-DD HH:mm:ss')
+  return info
+})
 
 export const createMyLogger = () => WinstonModule.createLogger({
     instance: createLogger({
       transports : [
         new winston.transports.Console({
           format: winston.format.combine(
-            winston.format.timestamp(),
+            customTimestamp(),
+            // winston.format.timestamp(),
             winston.format.ms(),
             nestWinstonModuleUtilities.format.nestLike('MyApp', {
               colors: true,
               prettyPrint: true,
-
             }),
           ),
         }),
@@ -27,9 +35,10 @@ export const createMyLogger = () => WinstonModule.createLogger({
           maxSize: '20m',
           maxFiles: '14d',
           format: winston.format.combine(
-            winston.format.timestamp(),
+            // winston.format.timestamp(),
+            customTimestamp(),
             winston.format.printf(({ timestamp, level, message }) => {
-              return `${timestamp} [${level}] ${message}`;
+              return `[${timestamp}] [${level}] ${message}`;
             }),
           ),
         }),
