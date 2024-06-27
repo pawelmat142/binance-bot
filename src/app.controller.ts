04375a0fae8620343496fdd9f93a9e9c1a5451bf
pawelmat142 <pawelmat142@gmail.com>
2024-06-27 18:10:11 +0200

    turn on receive telegram signal messages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TelegramMessage } from './telegram/message';
+import { SignalService } from './signal/signal.service';
 
 @Controller()
 export class AppController {
@@ -8,11 +10,18 @@ export class AppController {
 
   constructor(
     private readonly appService: AppService,
+    private readonly signalService: SignalService,
   ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // SIGNALS INPUT
+  @Post('/signal/telegram')
+  postTelegramMessage(@Body() body: TelegramMessage) {
+    return this.signalService.onReceiveTelegramMessage(body)
   }
 
 }
